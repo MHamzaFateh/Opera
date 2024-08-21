690d097724475997660e82a6d1e02e2fc61a0f08
MHamzaFateh <155734571+MHamzaFateh@users.noreply.github.com>
2024-08-21 22:16:33 +0500

    Update streamApp.py

--- streamApp.py
+++ streamApp.py
@@ -1,11 +1,20 @@
+# import streamlit as st
+# from st_audiorec import st_audiorec
+
+# st.title("Audio Recorder in Streamlit")
+
+# # Create an instance of the audio recorder
+# wav_audio_data = st_audiorec()
+
+# # If audio data is available, play it back
+# if wav_audio_data is not None:
+#     st.audio(wav_audio_data, format='audio/wav')
+
 import streamlit as st
-from st_audiorec import st_audiorec
+from audio_recorder_streamlit import audio_recorder
 
 st.title("Audio Recorder in Streamlit")
 
-# Create an instance of the audio recorder
-wav_audio_data = st_audiorec()
-
-# If audio data is available, play it back
-if wav_audio_data is not None:
-    st.audio(wav_audio_data, format='audio/wav')
+audio_bytes = audio_recorder()
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
